ab8517b5b8f8ae9e3ac92afc4bb0d358cb0bfb55
ZYEYT <71366015+ZYEYT@users.noreply.github.com>
2021-10-23 17:56:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const quotes = [
+{
+    name:'Stephen King',
+    quote:'Get busy living Or get busy dying.'
+},
+{
+    name:'John F.Kennedy',
+    quote:'Those who dare to fail miserably can be achieve greatly.'
+},
+{
+    name:'Abraham Lincoln',
+    quote:"I'm a succes today because I had a friend who believed in me and I didnt have the heart to let him down."
+},
+{
+name:"It had long since come to my attention that people of accomplishment rarely sat back ant let things happen to them.They went out and happened to things."
+}
+, 
+{
+name:"Leo Tolstoy",
+quote:"If you want to be happy,be."
+}
+]
+const quoteBtn = document.querySelector('quoteBtn');
+const quoteAuthor = document.querySelector
+('#quoteAuthor'),
